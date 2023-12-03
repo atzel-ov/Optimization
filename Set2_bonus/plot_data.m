6c4332834403c99d75f9dc1a4183740d971435d6
Angelopoulos Dimitris <83914255+atzel-ov@users.noreply.github.com>
2023-12-03 14:43:02 +0200

    Add files via upload

--- Set2_bonus/plot_data.m
+++ Set2_bonus/plot_data.m
@@ -1,4 +1,4 @@
-function [] = plot_data(w, b, y, X)
+function [] = plot_data(w, b, y, X, str)
 
 figure
 hold on
@@ -13,6 +13,10 @@ end
 plot_x = [min(X(:))-3,  max(X(:))+3];
 plot_y = -(w(1)/w(2))*plot_x + b/w(2);
 plot(plot_x, plot_y,'k', LineWidth=1.1)
+if(strcmp(str,'svm'))
+plot(plot_x, plot_y + 1/w(2),'--k', LineWidth=0.6)
+plot(plot_x, plot_y - 1/w(2),'--k', LineWidth=0.6)
+end
 axis ([min(X(:)) max(X(:)) min(X(:)) max(X(:))])
 xlabel('$x_1$', 'Interpreter','latex')
 ylabel('$x_2$', 'Interpreter','latex')
